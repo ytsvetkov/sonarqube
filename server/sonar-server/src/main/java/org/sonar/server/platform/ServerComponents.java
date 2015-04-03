da7c1cdc1a9a7c60f434a20cd7eb69e4b809463a
Jean-Baptiste Lievremont <jean-baptiste.lievremont@sonarsource.com>
2015-04-03 15:08:33 +0200

    SONAR-6305 WS to restore a quality profile

--- server/sonar-server/src/main/java/org/sonar/server/platform/ServerComponents.java
+++ server/sonar-server/src/main/java/org/sonar/server/platform/ServerComponents.java
@@ -383,6 +383,7 @@ class ServerComponents {
     pico.addSingleton(QProfileSetDefaultAction.class);
     pico.addSingleton(QProfileProjectsAction.class);
     pico.addSingleton(QProfileBackupAction.class);
+    pico.addSingleton(QProfileRestoreAction.class);
     pico.addSingleton(QProfilesWs.class);
     pico.addSingleton(ProfilesWs.class);
     pico.addSingleton(RuleActivationActions.class);
